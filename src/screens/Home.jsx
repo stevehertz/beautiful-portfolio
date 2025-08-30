355e50cce83b0d436cbd91e321b31052ce844f3a
stevehertz <stevekamahertz@gmail.com>
2025-08-29 20:37:54 -0400

    TailwindCSS Setup and Theme Customization

---
+++ src/screens/Home.jsx
@@ -0,0 +1,19 @@
+const Home = () => {
+  return (
+    <div className="min-h-screen">
+
+        {/* Theme Toggle */}
+
+        {/* Background Effects */}
+
+        {/* Navbar */}
+
+        {/* Main Content */}
+
+        {/* Footer */}
+
+    </div>
+  )
+}
+
+export default Home
